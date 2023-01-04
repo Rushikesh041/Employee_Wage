d9bdcd2b75c5c6530ee332efd8f499dca8e393cc
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-04 20:48:57 +0530

    [Rushi]UC5_MonthlyWage

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -2,15 +2,12 @@
 {
     public class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
+        public const int IS_FULL_TIME = 1, IS_PART_TIME = 2, EMP_RATE_PER_HOUR = 20, NUM_OF_WORKING_DAYS = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Wel Come to Employee Wage Computation");
             // Variables
-            int empHrs = 0;
-            int empWage = 0;
+            int empHrs = 0, empWage = 0, totalempwage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 3);
@@ -27,8 +24,10 @@
                     break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
+            totalempwage = empWage * NUM_OF_WORKING_DAYS;
             Console.WriteLine("Emp Wage : " + empWage);
+            Console.WriteLine("Total Emp Wage : " + totalempwage);
         }
     }
-    }
+    
 }
