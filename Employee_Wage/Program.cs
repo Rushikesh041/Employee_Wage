f38a06cc870e27b4546d65af40683ff2f6559b0d
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-04 20:35:23 +0530

    [Rushi]UC2_DailyWage

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -7,17 +7,23 @@
             Console.WriteLine("Wel Come to Employee Wage Computation");
             //Constants
             int IS_FULL_TIME = 1;
+            int EMP_RATE_PER_HOUR = 20;
+            // Variables
+            int empHrs = 0;
+            int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage : " + empWage);
         }
     }
 }
