819df2fe052df4bfd3c00b3154886dd6a48c45f8
Rushikesh041 <jadhavrushi041@gmail.com>
2023-01-04 20:43:25 +0530

    [Rushi]UC4_SwitchCase

--- Employee_Wage/Program.cs
+++ Employee_Wage/Program.cs
@@ -2,33 +2,33 @@
 {
     public class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
             Console.WriteLine("Wel Come to Employee Wage Computation");
-            //Constants
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 20;
             // Variables
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             //Computation
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_PART_TIME)
+            switch (empCheck)
             {
-                empHrs = 4;
-            }
-            else if (empCheck == IS_FULL_TIME)
-            {
-                empHrs = 8;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Emp Wage : " + empWage);
         }
+    }
     }
 }
